23913abd543bf8eb9bc52b878af30ec86a6388d4
Brokeice123 <keithmarkomen@gmail.com>
2024-03-01 21:43:47 +0000

    Select some Amenities to be comfortable

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  // initializing an empty dictionary
+  const dict = {};
+
+  // selecting the checkboxe and the h4 tag inside the div with class "amenities"
+  const $amenitiesCheck = $('input[type=checkbox]');
+  const $selectedAmenities = $('div.amenities h4');
+
+  // listen for click events on checkboxes
+  $amenitiesCheck.click(function () {
+    // if the checbox is checked
+    if ($(this).is(':checked')) {
+      // store Amenity ID in the dictionary
+      dict[$(this).data('id')] = $(this).data('name');
+      $selectedAmenities.text(Object.values(dict).join(', '));
+    } else if ($(this).is(':not(:checked)')) {
+      // remove Amenity ID from the dictionary
+      delete dict[$(this).data('id')];
+
+      // Finally update the h4 tag with selected ammenities
+      $selectedAmenities.text(Object.values(dict).join(', '));
+    }
+  });
+});
